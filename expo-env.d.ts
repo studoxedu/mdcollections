e7993ce77cfa70ab841b95625c96dc5bfbdbaf91
studoxedu <studox.edu@gmail.com>
2026-09-13 17:57:44 +0000

    update

--- expo-env.d.ts
+++ expo-env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="expo/types" />
 
-declare namespace NodeJS { interface ProcessEnv { EXPO_PUBLIC_SUPABASE_URL?: string; EXPO_PUBLIC_SUPABASE_ANON_KEY?: string; } }
-declare const process: { env: NodeJS.ProcessEnv };
+// NOTE: This file should not be edited and should be in your git ignore
